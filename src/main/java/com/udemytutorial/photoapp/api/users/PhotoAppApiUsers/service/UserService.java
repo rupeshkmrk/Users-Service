b05bca5bdf594e0b04ed4be9f9372e968ea0acd1
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 15:40:29 -0800

    Implement service layer and create shared DTO class

---
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserService.java
@@ -0,0 +1,8 @@
+package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.service;
+
+import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.shared.UserDto;
+
+public interface UserService {
+
+    UserDto createUser(UserDto userDto);
+}
---
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.service;
+
+import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.shared.UserDto;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+}
