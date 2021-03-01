79de079d4efc664f5f203440344703e556549bbf
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 15:44:17 -0800

    Generate user id

--- src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserServiceImpl.java
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserServiceImpl.java
@@ -2,10 +2,13 @@ package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.service;
 
 import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.shared.UserDto;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService{
 
     @Override
-    public UserDto createUser(UserDto userDto) {
+    public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(String.valueOf(UUID.randomUUID()));
         return null;
     }
 }
