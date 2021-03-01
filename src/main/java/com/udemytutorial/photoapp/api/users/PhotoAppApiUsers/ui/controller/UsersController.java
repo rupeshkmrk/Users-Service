2d5deff748523909ca521dec9f5c4e7232544ab8
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 15:23:14 -0800

    CreateUserRequestModel and Validating HTTP Request Body

--- src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/ui/controller/UsersController.java
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/ui/controller/UsersController.java
@@ -1,9 +1,14 @@
 package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.ui.controller;
 
+import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.ui.model.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -15,5 +20,10 @@ public class UsersController {
     @RequestMapping("/status/check")
     public String status() {
         return "User service running on port " + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody @Valid CreateUserRequestModel userRequestModel) {
+        return "create user called";
     }
 }
---
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/ui/model/CreateUserRequestModel.java
@@ -0,0 +1,56 @@
+package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.ui.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CreateUserRequestModel {
+
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, max = 50, message = "First name must not be less than 2 character or greater than 50")
+    private String firstName;
+
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, max = 50, message = "First name must not be less than 2 character or greater than 50")
+    private String lastName;
+
+    @NotNull(message = "Email must not be null")
+    @Email
+    private String email;
+
+    @NotNull(message = "Password must not be null")
+    @Size(min = 8, max = 16, message = "Password must be greater or equal to 8 and less than or equal to 16")
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
