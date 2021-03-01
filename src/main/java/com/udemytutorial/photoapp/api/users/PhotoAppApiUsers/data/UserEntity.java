029e6f21d2329aba3e489a94250f4d918094c009
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 15:54:52 -0800

    Added User entity

---
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/data/UserEntity.java
@@ -0,0 +1,68 @@
+package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "email", length = 120, unique = true)
+    private String email;
+
+    @Column(nullable = false, name = "user_id", unique = true)
+    private String userId;
+
+    @Column(name = "encrypted_password", nullable = false)
+    private String encryptedPassword;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+}
