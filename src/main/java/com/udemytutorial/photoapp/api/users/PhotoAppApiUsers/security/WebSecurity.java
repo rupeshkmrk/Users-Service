ef8529c3449e7c972b6bc7a7db04f8968ec627d2
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 22:39:26 -0800

    Aloow only ip address of api gateway

--- src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/security/WebSecurity.java
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,14 +19,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/users/**")
-                .permitAll()
+                .antMatchers("/**")
+                .hasIpAddress(environment.getProperty("gateway.ip"))
         ;
     }
 }
