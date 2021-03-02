92734bbcf2bf69fbd482a26d7a721dc51abcfc5a
Rupesh <rupeshkumar9312@gmail.com>
2021-03-01 22:24:02 -0800

    Encrypt User Password

--- src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -3,13 +3,21 @@ package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PhotoAppApiUsersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBean() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
+
--- src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserServiceImpl.java
+++ src/main/java/com/udemytutorial/photoapp/api/users/PhotoAppApiUsers/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.service;
 
+import com.netflix.discovery.converters.Auto;
 import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.data.UserEntity;
 import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.data.UserRepository;
 import com.udemytutorial.photoapp.api.users.PhotoAppApiUsers.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,18 +18,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
     @Override
     public UserDto createUser(UserDto userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
 
-        //TODO : to be removed
-        userEntity.setEncryptedPassword("test");
+        userEntity.setEncryptedPassword(passwordEncoder.encode(userDetails.getPassword()));
         userEntity.setUserId(String.valueOf(UUID.randomUUID()));
         userRepository.save(userEntity);
 
-        UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
-        return returnValue;
+        return modelMapper.map(userEntity, UserDto.class);
     }
 }
